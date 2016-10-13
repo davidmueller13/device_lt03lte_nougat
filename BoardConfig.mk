fcaac723b71466c7ba918c0950e1ff7a096d103a
davidmueller13 <david.mueller1013@gmail.com>
2016-10-13 12:14:59 +0200

    Use multithread decode for boot animation
    This gets rid of the jerkiness in the boot animation.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,3 +113,6 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
 
+# Display
+TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
+
