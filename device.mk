3b1d6f893ded3a75f2e9e25603ed7fe807d183f9
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    lt03lte: Add libbt-vendor in klte.mk
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b
    
    # Conflicts:
    #	klte.mk

--- device.mk
+++ device.mk
@@ -61,7 +61,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
 
 # BoringSSL compatability wrapper
 PRODUCT_PACKAGES += \
