c506bbbbea8890b82936f4319d85585ce181dc06
David Müller <david.mueller1013@gmail.com>
2017-07-03 21:46:29 +0200

    Revert "switch to sony timekeep"
    This reverts commit d3b6018640c5c227b0305f92d4aaa3957433f0d9.

--- device.mk
+++ device.mk
@@ -146,11 +146,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
 
-# Time
-PRODUCT_PACKAGES += \
-    timekeep \
-    TimeKeep
-
 # WiFi
 PRODUCT_PACKAGES += \
     libnetcmdiface \
