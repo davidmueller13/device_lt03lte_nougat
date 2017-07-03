2c26515f27de09f9b52794b94c31f66b35111998
David Müller <david.mueller1013@gmail.com>
2017-07-03 21:45:05 +0200

    update RIL, fix compiling
    Change-Id: I80a1462252b1b17b522325cdbbbc5b25bf4861fc

--- init/init_lt03lte.cpp
+++ init/init_lt03lte.cpp
@@ -30,6 +30,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
@@ -38,6 +41,17 @@
 #include "init_msm8974.h"
 
 #define ISMATCH(a, b) (!strncmp((a), (b), PROP_VALUE_MAX))
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 void init_target_properties()
 {
