5ce405956f89cf78d0b2543d596908b827e657ca
David Müller <david.mueller1013@gmail.com>
2017-08-29 17:30:45 +0200

    add hidl halls
    Change-Id: Id7eb232aa28ae061b2da07c1ac111862b7ec4a30

--- device.mk
+++ device.mk
@@ -180,21 +180,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.service.debuggable=1 \
     ro.adb.secure=0
 
-# Temp Additions
-PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.bluetooth@1.0-impl \
-	android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.light@2.0-impl \
-    android.hardware.power@1.0-impl \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.wifi@1.0-service \
-
 # Common msm8974
 $(call inherit-product, device/samsung/msm8974-common/msm8974.mk)
+
+# HIDL
+$(call inherit-product, $(LOCAL_PATH)/hidl.mk)
