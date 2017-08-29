9b32a901c98605e135613c21a5c89afe7eaea526
David Müller <david.mueller1013@gmail.com>
2017-08-29 12:04:12 +0200

    now kernel has CLOCK_BOOTTIME_ALARM support
    Change-Id: Id189a45c4dd8a54bd9444fcd7e46426e1049c739

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,7 +23,4 @@
 #define BLE_VND_INCLUDED TRUE
 #define BTM_WBS_INCLUDED TRUE
 
-/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
-
 #endif
