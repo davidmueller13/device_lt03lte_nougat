4c9931cd110b57299f99736d1af5544456e88cdd
David Müller <david.mueller1013@gmail.com>
2017-09-13 17:11:27 +0200

    Revert "shim mpdecision"
    This reverts commit c238c1e04a287ffe0a518b4510092b167252aa11.

--- device.mk
+++ device.mk
@@ -99,10 +99,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
-# MpDecision
-PRODUCT_PACKAGES += \
-    libshim_atomic
-
 # MSM IPC Router security configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
