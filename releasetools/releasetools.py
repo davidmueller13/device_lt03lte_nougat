4c9fe6a0917240a550e664bb8efdd40db70f5dd8
David Müller <david.mueller1013@gmail.com>
2017-09-20 22:22:33 +0200

    remove temp build fix
    Change-Id: I883986f6c0f653817631eb24b2572443f996c2f6

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -22,5 +22,5 @@ def FullOTA_InstallEnd(info):
   info.script.AppendExtra('ifelse(is_substring("P607T", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox cp /system/lib/gsm/* /system/lib/"));')
   info.script.AppendExtra('ifelse(is_substring("P605", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox cp /system/lib/gsm/* /system/lib/"));')
   info.script.AppendExtra('ifelse(is_substring("P605V", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox cp /system/lib/cdma/* /system/lib/"));')
-#  info.script.AppendExtra('delete_recursive("/system/lib/gsm/");')
-#  info.script.AppendExtra('delete_recursive("/system/lib/cdma/");')
+  info.script.AppendExtra('delete_recursive("/system/lib/gsm/");')
+  info.script.AppendExtra('delete_recursive("/system/lib/cdma/");')
