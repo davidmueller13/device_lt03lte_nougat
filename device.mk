d3fb2f01310bc038f97c418f30345d456585bd4f
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-11 19:53:28 +0300

    lt03lte: Add USB service
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba36
    
    # Conflicts:
    #	device.mk

--- device.mk
+++ device.mk
@@ -171,6 +171,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+    
 # WiFi
 PRODUCT_PACKAGES += \
     libnetcmdiface \
