4bb23ceefae0d36fb14adbf1698db8464f531100
Kevin F. Haggerty <haggertk@lineageos.org>
2017-11-06 06:51:08 -0700

    lt03lte: Remove libsecril-client*
    * These are only required for some OEM blobs, and not even any
      that we make use of:
      * system/bin/smdexe matches
      * system/bin/vpnclientd matches
      * system/bin/connfwexe matches
      * system/bin/at_distributor matches
      * system/bin/wpa_supplicant matches
      * system/etc/irremovable_list.txt:/system/lib/libsecril-client.so
      * system/lib/hw/lights.msm8974.so matches
      * system/lib/hw/audio.primary.msm8974.so matches
      * system/lib/libcpve-client.so matches
      * system/lib/libaudio-ril.so matches
      * system/lib/libsecril-client.so matches
    
    Change-Id: I7488681c9c1c15c1a42ba9a718c426c80526db19
    
    # Conflicts:
    #	klte.mk

--- device.mk
+++ device.mk
@@ -135,9 +135,7 @@ PRODUCT_PACKAGES += \
 # Radio
 PRODUCT_PACKAGES += \
 	libreference-ril \
-	libsecnativefeature \
-    libsecril-client \
-    libsecril-client-sap \
+	libsecnativefeature
 
 # Ramdisk
 PRODUCT_PACKAGES += \
