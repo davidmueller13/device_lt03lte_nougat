0f4cea6e4dc07fd7dd53c020beddcaf4aad1eb7a
Kevin F. Haggerty <haggertk@lineageos.org>
2017-12-24 10:29:49 -0700

    lt03lte: Move proprietary binaries to vendor
    Change-Id: If8f69a66f6168869e2a7578110d1200510b73ddd
    
    # Conflicts:
    #	BoardConfig.mk
    #	common-proprietary-files.txt
    #	extract-files.sh
    #	rootdir/etc/init.qcom.rc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,11 +127,11 @@ BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/system/etc/wifi/bcmdhd_apsta.bin"
 
-WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/vendor/system/etc/wifi/bcmdhd_sta.bin nvram_path=/vendor/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/vendor/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/vendor/system/etc/wifi/nvram_net.txt"
 
 # Display
 VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
