3ad24db09aa72af4173f3caafa6d54d8ede0a754
davidmueller13 <david.mueller1013@gmail.com>
2018-02-17 13:27:57 +0100

    update media profiles/codecs
    Change-Id: I067bd6851456c019dda0606f7927419731dd4287

--- device.mk
+++ device.mk
@@ -92,7 +92,8 @@ PRODUCT_PACKAGES += \
 
 # Media profiles
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 # MSM IPC Router security configuration
 PRODUCT_COPY_FILES += \
