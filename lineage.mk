a0888eafdd58dfb156b77a54c05f02946606366a
davidmueller13 <david.mueller1013@gmail.com>
2018-05-23 00:02:47 +0200

    lt03lte: Fix vendor image mismatch
    Change-Id: I5520a77ca6c91f49a439013bffe4c309b0f26ea4

--- lineage.mk
+++ lineage.mk
@@ -7,3 +7,6 @@ BOARD_VENDOR := samsung
 PRODUCT_DEVICE := lt03lte
 PRODUCT_NAME := lineage_lt03lte
 TARGET_VENDOR := samsung
+
+
+BUILD_FINGERPRINT := samsung/lt03ltexx/lt03lte:4.4.2/KOT49H/P605XXUDOB1:user/release-keys
