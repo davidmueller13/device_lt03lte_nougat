ea6b95a3f503fbbea6b05c684e7e430ad96b0064
davidmueller13 <david.mueller1013@gmail.com>
2019-08-01 15:11:29 +0200

    add wifi configs
    Change-Id: I4d010011090ed1d2a4b6978acb9eb8552eb0b411

--- device.mk
+++ device.mk
@@ -164,6 +164,11 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+$(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+$(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+
 # Allow lockscreen rotation
 PRODUCT_PROPERTY_OVERRIDES += \
     lockscreen.rot_override=true
