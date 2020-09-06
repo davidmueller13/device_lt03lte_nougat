3563ad17db3ef4683f9afc8323b4ede270f0a108
David Mueller <david.mueller1013@gmail.com>
2020-09-06 12:21:42 +0200

    update
    Change-Id: If46e9395310ad39d378ee0ee3c95726dbcd0f880

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012-2016, The CyanogenMod Project
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2017-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -72,8 +72,8 @@ camera_module_t HAL_MODULE_INFO_SYM = {
          .module_api_version = CAMERA_MODULE_API_VERSION_1_0,
          .hal_api_version = HARDWARE_HAL_API_VERSION,
          .id = CAMERA_HARDWARE_MODULE_ID,
-         .name = "MSM8974 Camera Wrapper",
-         .author = "The CyanogenMod Project",
+         .name = "LT03LTE Camera Wrapper",
+         .author = "The LineageOS Project",
          .methods = &camera_module_methods,
          .dso = NULL,
          .reserved = {0},
